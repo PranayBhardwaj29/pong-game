d88882e27ed00757bb210816a406bfb753414d58
PranayBhardwaj29 <235537439+PranayBhardwaj29@users.noreply.github.com>
2026-08-07 20:00:29 +0530

    Added paddle1, paddle2 and ball

--- include/Game.h
+++ include/Game.h
@@ -5,6 +5,8 @@
 class Game {
 private:
     sf::RenderWindow window;
+    sf::RectangleShape paddle1, paddle2;
+    sf::CircleShape ball;
 
 public:
     Game();
--- src/Game.cpp
+++ src/Game.cpp
@@ -4,13 +4,38 @@ Game::Game()
     : window(sf::VideoMode({1600, 900}), "Pong")
 {
     
+    // Paddle 1
+    paddle1.setSize({20.f, 150.f});
+    paddle1.setPosition({50.f, 375.f});
+    paddle1.setFillColor(sf::Color::White);
+    
+    // Paddle 2
+    paddle2.setSize({20.f, 150.f});
+    paddle2.setPosition({1530.f, 375.f});
+    paddle2.setFillColor(sf::Color::White);
+    
+    // Ball
+    ball.setRadius(10.f);
+    ball.setPosition({790.f, 440.f});
+    ball.setFillColor(sf::Color::White);
 }
 
 void Game::run()
 {
     while (window.isOpen())
     {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
         
+        window.clear();
+
+        window.draw(paddle1);
+        window.draw(paddle2);
+        window.draw(ball);
+
+        window.display();
     }
-    
 }
